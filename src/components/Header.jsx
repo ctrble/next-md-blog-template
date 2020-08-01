102a265414b5d662e895bbf83095089d03b7d804
Charlotte Trible <ctrble@gmail.com>
2020-08-01 17:24:42 -0500

    adding some nav styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,14 @@ import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.header}>
-    <Link href={'/'}>
-      <a>Home</a>
-    </Link>
+    <nav className={styles.nav}>
+      <Link href={'/'}>
+        <a className={styles.link}>Home</a>
+      </Link>
+      <Link href={'/about'}>
+        <a className={styles.link}>About</a>
+      </Link>
+    </nav>
     <Search />
   </header>
 );
